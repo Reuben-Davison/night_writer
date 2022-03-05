b25b1963b6b6b4df1fc928395fe5d2c99f764717
reuben <creubend@gmail.com>
2022-03-05 13:07:57 -0700

    feat: refactored text editor to be able to see input from any method

--- lib/text_editor.rb
+++ lib/text_editor.rb
@@ -2,19 +2,22 @@ require "./convertable"
 
 class TextEditor
   include Convertable
+  attr_reader :message
   
   def initialize(input, output)
     @input  = File.open(input, "r")
     @output = output
+    @message = nil
+    input_message
     # prints_message
   end
 
   def input_message
-    @input.read
+    @message = @input.read.chomp
   end
 
   def prints_message 
-    print "Created #{@output} containing #{input_message.length} characters"
+    print "Created #{@output} containing #{@message.length} characters"
   end
 
 
