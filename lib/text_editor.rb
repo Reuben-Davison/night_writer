cba8a27c5e4cc15a239f0eebae1bc28efc3ea7c8
reuben <creubend@gmail.com>
2022-03-06 09:22:47 -0700

    test: converts letters to braille characters

--- lib/text_editor.rb
+++ lib/text_editor.rb
@@ -19,6 +19,22 @@ class TextEditor
   def prints_message 
     print "Created #{@output} containing #{@message.length} characters"
   end
+  
+  def incoming_message_to_array
+    @message.split("")
+  end
+  
+  def convert_to_braille
+    number = 0
+    incoming_message_to_array.each {|letter| "#{converter_key["letter"][number]}"}
+    require "pry"; binding.pry
+    3.times do
+      message = "#{hash[“a”][number]}#{hash[“b”][number]}#{}#{}#{}#{}"
+      number += 1
+    end
+  
+    
+  end
 
 
 end 
--- spec/text_editor_spec.rb
+++ spec/text_editor_spec.rb
@@ -24,6 +24,11 @@ RSpec.describe TextEditor do
       # require "pry"; binding.pry
       expect(@te.library).to eq(@te.converter_key)
     end
+    it "splits the incoming message into an array" do
+      expect(@te.incoming_message_to_array).to eq(["H", "e", "l", "l", "o", " ", "W", "o", "r", "l", "d", " ", "t", "h", "i", "s", " ", "i", "s", " ", "a", " ", "t", "e", "s", "t", " ", "f", "i", "l", "e"])
+      
+    end
+    
     it "converts letters to braille " do
       expect(@te.convert_to_braille("abc")).te eq("0.0.00\n..0...\n......")
       
