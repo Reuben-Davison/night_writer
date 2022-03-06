2f9f5bdbf02dffbe55dd475cda4bd9c7b08bd1c8
reuben <creubend@gmail.com>
2022-03-06 12:15:14 -0700

    feat: file created with the correct output name and braille conversion

--- lib/text_editor.rb
+++ lib/text_editor.rb
@@ -12,15 +12,23 @@ class TextEditor
     @braille_message = nil
     @converter_key = {}
     input_message
+    output_message
   end
 
   def input_message
     @message = @input.read.chomp
   end
+  
+  def output_message
+    convert_to_braille
+    # require "pry"; binding.pry
+    @output_location.write(@braille_message)
+  end
 
   def convert_to_braille
-    number = 0
-    lines_of_braille = []
+    library
+    lines_of_braille = [] 
+    number = 0 
     3.times do
       lines_of_braille << incoming_message_to_array.map {|letter| "#{converter_key[letter][number]}"}.join
       number += 1
